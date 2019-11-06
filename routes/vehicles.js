47345fa383be7f9f5d9bc4ac7d0c750d1b1df703
Michael Haberler <git@mah.priv.at>
2019-11-06 04:33:54 +0100

    vehicles.js: first stab at vehicle crud API
    Signed-off-by: Michael Haberler <git@mah.priv.at>

---
+++ routes/vehicles.js
@@ -0,0 +1,208 @@
+'use strict';
+
+// API router for vehicle resources
+var passport = require('passport');
+var MAX_VEHICLES = 500;
+
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+
+var schemas = require('../schemas/schemas').schemas;
+var validator = require('is-my-json-valid');
+var shortid = require('shortid');
+//var path    = require('path');
+
+
+module.exports = function(router, db) {
+
+    var isValidToken = passport.authenticate('jwt', {
+        session: false
+    });
+
+    // retrieve number of vehicles
+    router.get('/v1/vehicles/number', function(req, res) {
+        logger.info('get number of vehicles');
+        db.collection('vehicles', function(err, collection) {
+            //var query = buildTracksQuery(req.query);
+            collection.find(req.query).count(function(err, count) {
+                logger.info('number of vehicles is...', count);
+                if (err) {
+                    logger.error('database error', err.message);
+                    res.status(507).send({
+                        error: 'DatabaseQueryError',
+                        description: err.message
+                    });
+                }
+                else {
+                    res.send({
+                        numberOfVehicles: count
+                    });
+                }
+            });
+        });
+    });
+
+    // Create a vehicle (must have valid token to succeed)
+    router.post('/v1/vehicles', isValidToken, function(req, res) {
+        logger.info('add vehicle', req.body);
+        var v = validator(schemas.vehicleRegistrationSchema);
+        if (v(req.body)) {
+            var vehicle = {};
+            vehicle.name = req.body.name;
+            vehicle.createdDate = new Date();
+            vehicle.vehicleId = shortid.generate();
+            vehicle.blob = Buffer.from(req.body.blob, 'base64');
+            vehicle.description = req.body.description;
+            vehicle.owner = req.body.owner;
+
+            db.collection('vehicles').insert(vehicle, {
+                w: 1
+            }, function(err) {
+                if (err) {
+                    if (err.code === 11000) {
+                        res.status(422).send({
+                            error: 'Duplicate',
+                            description: 'vehicle already exists'
+                        });
+                    }
+                    else {
+                        logger.error('database error', err.code);
+                        res.status(507).send({
+                            error: 'DatabaseInsertError',
+                            description: err.message
+                        });
+                    }
+                }
+                else {
+                    res.status(200).send({
+                        name: vehicle.name,
+                        id: vehicle.vehicleId
+                    });
+                }
+            });
+
+        }
+        else {
+            logger.error('validator ', v.errors);
+            res.status(400).send({
+                error: 'InvalidInput',
+                'description': v.errors
+            });
+        }
+    });
+
+    // retrieve vehicle info, including blob if ?blob=x is given
+    router.get('/v1/vehicles/:vehicleId', function(req, res) {
+        var vehicleId = req.params.vehicleId;
+        logger.info('retrieve vehicle: ' + vehicleId);
+        db.collection('vehicles', function(err, collection) {
+            collection.findOne({
+                'vehicleId': vehicleId
+            }, {
+                _id: true,
+                blob: req.query.blob,
+                description: true,
+                owner: true,
+                createdDate: true
+            }, function(err, item) {
+                if (item) {
+                    res.send(item);
+                }
+                else {
+                    logger.error('vehicle ' + vehicleId + ' not found');
+                    res.status(404).json({
+                        error: 'NotFound',
+                        description: 'vehicle not found'
+                    });
+                }
+            });
+        });
+    });
+
+    // retrieve all vehicles, including blob if ?blob=x is given
+    router.get('/v1/vehicles/', function(req, res) {
+        logger.info('retrieve vehicles: ' + req.query);
+        db.collection('vehicles', function(err, collection) {
+            var p = {
+                //_id: true,
+                // blob: false, // req.query.blob == 'true',
+                vehicleId: true,
+                name: true,
+                description: true,
+                owner: true,
+                createdDate: true
+            };
+            collection.find({}, {
+                limit: MAX_VEHICLES,
+                sort: {
+                    createdDate: -1
+                },
+                fields: p
+            }, function(err, stream) {
+                var result = {};
+                result.vehicles = {};
+                stream.on('data', function(data) {
+                    result.vehicles[data.vehicleId] = data;
+                });
+                stream.on('end', function() {
+                    if (Object.keys(result.vehicles).length === 0) {
+                        logger.info('no vehicles found');
+
+                        // res.status(404).send({error: 'NotFound', description: 'query returned no data'});
+                        // res.status(204).send();
+                        res.status(204).send({
+                            vehicles: {}
+                        });
+                    }
+                    else {
+                        res.send(result);
+                    }
+                });
+            });
+        });
+    });
+
+    // Delete a vehicle by id (must have valid token to succeed)
+    router.delete('/v1/vehicles/:vehicleId', isValidToken, function(req, res) {
+        var vehicleId = req.params.vehicleId;
+        logger.info('delete vehicle: ' + vehicleId);
+        db.collection('vehicles', function(err, vehiclesCollection) {
+            vehiclesCollection.findOne({
+                $and: [{
+                    'vehicleId': vehicleId
+                }, {
+                    'owner': req.user
+                }]
+            }, {
+                _id: false,
+                vehicleId: true
+            }, function(err, item) {
+                if (item) {
+                    vehiclesCollection.remove({
+                        'vehicleId': vehicleId
+                    }, {
+                        w: 1
+                    }, function(err) {
+                        if (err) {
+                            logger.error('database error', err.code);
+                            res.status(507).send({
+                                error: 'DatabaseDocRemoveError',
+                                description: err.message
+                            });
+                        }
+                        else {
+                            res.status(204).send();
+                        }
+                    });
+                }
+                else {
+                    res.status(507).send({
+                        error: 'vehicle not found',
+                        description: 'id: ' + vehicleId
+                    });
+                }
+            });
+        });
+    });
+
+};
