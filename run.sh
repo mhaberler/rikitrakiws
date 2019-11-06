9777e9305355f0dabe2e7e46264601dc7c682e16
Michael Haberler <git@mah.priv.at>
2019-11-06 17:07:29 +0100

    run.sh fix path squashme

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 . rikitrakiws.default
 
 #node rikitrakiws.js >rikitrakiws.log 2>&1
-#usr/bin/node  rikitrakiws.js
-/usr/bin/node --inspect-brk=127.0.0.1:9230  rikitrakiws.js
+#node  rikitrakiws.js
+node --inspect-brk=127.0.0.1:9230  rikitrakiws.js
