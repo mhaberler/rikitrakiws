92f17b0befa737640555efe31e88391b3037dc9e
Michael Haberler <git@mah.priv.at>
2019-11-29 01:41:36 +0100

    index.js: create indices on startup if none found
    so empty database works without loading
    
    make startup phase synchronous

--- routes/index.js
+++ routes/index.js
@@ -17,12 +17,62 @@ var JwtStrategy = require('passport-jwt').Strategy;
 
 var bcrypt = require('bcryptjs');
 
-mongoClient.connect(MONGO_URL, function(err, db) {
-	logger.info('connecting to database:', MONGO_URL);
-
-	if (err) {
-		logger.error('cannot connect to database');
+async function createIndices(db) {
+	let users = await db.createCollection('users');
+	if (!await users.indexExists('username_unique')) {
+		logger.info('creating index on users.username');
+		await users.createIndex({ 'username' : 1} , {unique:true, name: 'username_unique' });
 	}
+	if (!await users.indexExists('email_unique')) {
+		logger.info('creating index on users.email');
+		await users.createIndex({ 'email' : 1} , {unique:true, name: 'email_unique' });
+	}
+	let invitations = await db.createCollection('invitations');
+	if (!await invitations.indexExists('email_unique')) {
+		logger.info('creating index on invitations.email');
+		await invitations.createIndex({ 'email' : 1} , {unique:true, name: 'email_unique' });
+	}
+	let pictures = await db.createCollection('pictures');
+	if (!await pictures.indexExists('trackId_picIndex')) {
+		logger.info('creating index on pictures.trackId_picIndex');
+		await pictures.createIndex({ 'trackId' : 1, 'picIndex' : 1} , {unique:true, name: 'trackId_picIndex' });
+	}
+	let tracks = await db.createCollection('tracks');
+	if (!await tracks.indexExists('trackId_picIndex')) {
+		logger.info('creating index on tracks.trackId_picIndex');
+		await tracks.createIndex({ 'trackId' : 1} , {unique:true, name: 'trackId_unique' });
+	}
+	if (!await tracks.indexExists('trackGeoJson_idx')) {
+		logger.info('creating index on tracks.trackGeoJson');
+		await tracks.createIndex({ 'trackGeoJson' : '2dsphere'} , {name: 'trackGeoJson_idx' });
+	}
+	if (!await tracks.indexExists('createdDate_idx')) {
+		logger.info('creating index on tracks.createdDate');
+		await tracks.createIndex({ 'createdDate' : 1} , { name: 'createdDate_idx' });
+	}
+	if (!await tracks.indexExists('username_idx')) {
+		logger.info('creating index on tracks.username');
+		await tracks.createIndex({ 'username' : 1} , { name: 'username_idx' });
+	}
+	let vehicles = await db.createCollection('vehicles');
+	if (!await vehicles.indexExists('vehicles_idx')) {
+		logger.info('creating index on vehicles.name');
+		await vehicles.createIndex({ 'name' : 1} , {unique:true, name: 'vehicles_idx' });
+	}
+}
+
+(async function (url, dbname) {
+	logger.info('connecting to database:', url, dbname);
+
+	try {
+		var client = await mongoClient.connect(url);
+	} catch (err) {
+		logger.error('cannot connect to database', err);
+	    return "fail";
+	}
+	var db = client.db(dbname)
+
+	let c = await createIndices(db);
 
 	router.use(function (req, res, next) {
 		// Set up CORS headers
@@ -31,12 +81,7 @@ mongoClient.connect(MONGO_URL, function(err, db) {
 		res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-		if (err) {
-			logger.error('500 - cannot connect to database');
-			res.status(500).send({error: 'DBError', description: 'cannot connect to database'});
-		} else {
 		next();
-		}
 	});
 
 	// Setup basic authentication middleware
@@ -72,6 +117,11 @@ mongoClient.connect(MONGO_URL, function(err, db) {
 	require('./users')(router, db);
 	require('./tracks')(router, db);
 	require('./vehicles')(router, db);
+
+	return "success";
+
+})(MONGO_URL,MONGO_DB_NAME).then(v => {
+	logger.info('setup returned:',v);
 
 });
 
